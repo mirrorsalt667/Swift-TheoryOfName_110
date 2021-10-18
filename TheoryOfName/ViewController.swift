58fc6830fba2dbea9237d60cd918b9d99af66db3
mirrorsalt667 <79618127+mirrorsalt667@users.noreply.github.com>
2021-10-18 21:57:32 +0800

    Initial Commit

---
+++ TheoryOfName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheoryOfName
+//
+//  Created by 黃肇祺 on 2021/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
